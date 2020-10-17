a2885e93b87e640cd7582c970cb18b5eaf15329e
judgeou <judgeou@vip.qq.com>
2020-10-17 23:30:49 +0800

    重新整理代码

--- CharVideo/CharVideo.cpp
+++ CharVideo/CharVideo.cpp
@@ -2,12 +2,13 @@
 #include <string>
 #include <thread>
 #include <vector>
+#include <list>
 #include <chrono>
+#include <mutex>
+#include <condition_variable>
 
 #include <Windows.h>
-#include <gdiplus.h>
-#include <gdiplusgraphics.h>
-#pragma comment(lib, "Gdiplus.lib")
+
 
 extern "C" {
 #include <libavcodec/avcodec.h>
@@ -29,6 +30,26 @@ extern "C" {
 
 using std::string;
 using std::wstring;
+using std::vector;
+using std::list;
+
+using namespace std::chrono;
+
+int width;
+int height;
+std::mutex mut01;
+HDC hdc;
+
+struct FrameData
+{
+    vector<uint8_t> bit;
+    system_clock::time_point playTime;
+};
+list<FrameData> bitmapList;
+
+void addRgbbuffer(AVFrame* frame) {
+
+}
 
 string w2s(const wstring& wstr) {
     int len = WideCharToMultiByte(CP_ACP, 0, wstr.c_str(), wstr.size(), NULL, 0, NULL, NULL);
@@ -52,12 +73,43 @@ void save_gray_frame(unsigned char* buf, int wrap, int xsize, int ysize, char* f
     fclose(f);
 }
 
-int dealFrame(HDC hdc, char * infile, HWND hwnd) {
+void DrawFrame(const FrameData& bitData) {
+    BITMAPINFO info = { 0 };
+    auto& bgr_buffer = bitData.bit;
+    info.bmiHeader.biBitCount = 24;
+    info.bmiHeader.biWidth = width;
+    info.bmiHeader.biHeight = -height;
+    info.bmiHeader.biPlanes = 1;
+    info.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+    info.bmiHeader.biSizeImage = bgr_buffer.size();
+    info.bmiHeader.biCompression = BI_RGB;
+
+    std::this_thread::sleep_until(bitData.playTime);
+    StretchDIBits(hdc, 0, 0, width, height, 0, 0, width, height, &bgr_buffer[0], &info, DIB_RGB_COLORS, SRCCOPY);
+
+    {
+        COORD topLeft = { 0, 0 };
+        HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+        CONSOLE_SCREEN_BUFFER_INFO screen;
+        DWORD written;
+
+        GetConsoleScreenBufferInfo(console, &screen);
+
+        FillConsoleOutputAttribute(
+            console, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE,
+            screen.dwSize.X * screen.dwSize.Y, topLeft, &written
+        );
+        SetConsoleCursorPosition(console, topLeft);
+
+        printf("Hello World\nHello World\nHello World\nHello World\n");
+    }
+}
+
+int dealFrame(char * infile, HWND hwnd) {
     AVFormatContext* pFormatContext = avformat_alloc_context();
     avformat_open_input(&pFormatContext, infile, NULL, NULL);
 
     avformat_find_stream_info(pFormatContext, NULL);
-    printf("Format %s, duration %lld us\n", pFormatContext->iformat->long_name, pFormatContext->duration);
 
     for (int i = 0; i < pFormatContext->nb_streams; i++)
     {
@@ -66,9 +118,8 @@ int dealFrame(HDC hdc, char * infile, HWND hwnd) {
 
         // 只要视频和音频
         if (pLocalCodecParameters->codec_type == AVMEDIA_TYPE_VIDEO) {
-            int width = pLocalCodecParameters->width;
-            int height = pLocalCodecParameters->height;
-            printf("Video Codec: resolution %d x %d\n", width, height);
+            width = pLocalCodecParameters->width;
+            height = pLocalCodecParameters->height;
 
             AVCodecContext* pCodecContext = avcodec_alloc_context3(pLocalCodec);
             avcodec_parameters_to_context(pCodecContext, pLocalCodecParameters);
@@ -81,42 +132,39 @@ int dealFrame(HDC hdc, char * infile, HWND hwnd) {
             AVFrame* pFrame = av_frame_alloc();
             AVFrame* pRgbFrame = av_frame_alloc();
             int num_bytes = av_image_get_buffer_size(AV_PIX_FMT_BGR24, width, height, 1);
-            uint8_t* p_global_bgr_buffer = (uint8_t*)av_malloc(sizeof(uint8_t) * num_bytes);
 
-            av_image_fill_arrays(pRgbFrame->data, pRgbFrame->linesize, p_global_bgr_buffer,
-                AV_PIX_FMT_BGR24, width, height, 1);
-
-            auto startTime = std::chrono::system_clock::now();
+            auto startTime = system_clock::now();
 
             while (av_read_frame(pFormatContext, pPacket) >= 0) {
                 avcodec_send_packet(pCodecContext, pPacket);
                 avcodec_receive_frame(pCodecContext, pFrame);
 
                 if (pFrame->best_effort_timestamp >= 0) {
-                    auto pts = av_rescale_q(pFrame->best_effort_timestamp, pFormatContext->streams[i]->time_base, AV_TIME_BASE_Q);
-                    auto playTime = startTime + std::chrono::microseconds(pts);
+                    vector<uint8_t> bgr_buffer(num_bytes);
+                    auto p_global_bgr_buffer = &bgr_buffer[0];
 
-                    std::this_thread::sleep_until(playTime);
+                    av_image_fill_arrays(pRgbFrame->data, pRgbFrame->linesize, p_global_bgr_buffer,
+                        AV_PIX_FMT_BGR24, width, height, 1);
 
+                    int64_t pts = av_rescale_q(pFrame->best_effort_timestamp, pFormatContext->streams[i]->time_base, AV_TIME_BASE_Q);
+                    auto playTime = startTime + microseconds(pts);
+                    
                     sws_scale(swsContext, pFrame->data, pFrame->linesize, 0, height, pRgbFrame->data, pRgbFrame->linesize);
 
-                    BITMAPINFO info = { 0 };
-                    info.bmiHeader.biBitCount = 24;
-                    info.bmiHeader.biWidth = pFrame->width;
-                    info.bmiHeader.biHeight = -pFrame->height;
-                    info.bmiHeader.biPlanes = 1;
-                    info.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
-                    info.bmiHeader.biSizeImage = num_bytes;
-                    info.bmiHeader.biCompression = BI_RGB;
-
-                    StretchDIBits(hdc, 0, 0, width, height, 0, 0, width, height, p_global_bgr_buffer, &info, DIB_RGB_COLORS, SRCCOPY);
-
+                    DrawFrame({ bgr_buffer, playTime });
+                    /*if (bitmapList.size() >= 5) {
+                        mut01.lock();
+                        bitmapList.push_back({ bgr_buffer, playTime });
+                        mut01.unlock();
+                    }
+                    else {
+                        bitmapList.push_back({ bgr_buffer, playTime });
+                    }*/
                 }
 
 
             }
 
-            av_free(p_global_bgr_buffer);
             sws_freeContext(swsContext);
         }
     }
@@ -124,10 +172,33 @@ int dealFrame(HDC hdc, char * infile, HWND hwnd) {
     return 0;
 }
 
+void DrawVideoLoop() {
+    return;
+    while (1) {
+        if (bitmapList.size() >= 5) {
+            mut01.lock();
+            auto& bitData = bitmapList.front();
+            DrawFrame(bitData);
+
+            bitmapList.pop_front();
+            mut01.unlock();
+        }
+    }
+}
+
 int main(int argc, char * argv[])
 {
+    av_log_set_level(AV_LOG_QUIET);
+
     auto hInstance = GetModuleHandle(NULL);
     DWnd mainWind(hInstance, IDD_DIALOG1);
+
+    hdc = GetDC(mainWind.mainHWnd);
+
+    mainWind.AddMessageListener(WM_DESTROY, [](HWND hWnd, ...) {
+        // exit thread
+        ReleaseDC(hWnd, hdc);
+    });
 
     mainWind.AddCommandListener(IDCANCEL, [](HWND hWnd, ...) {
         DestroyWindow(hWnd);
@@ -135,12 +206,10 @@ int main(int argc, char * argv[])
 
     mainWind.AddCommandListener(IDOK, [argv](HWND hWnd, ...) {
         std::thread([hWnd, argv]() {
-            auto hdc = GetDC(hWnd);
-
-            dealFrame(hdc, argv[1], hWnd);
-
-            ReleaseDC(hWnd, hdc);
+            dealFrame(argv[1], hWnd);
         }).detach();
+
+        std::thread(DrawVideoLoop).detach();
     });
 
     mainWind.AddMessageListener(WM_DROPFILES, [](HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
@@ -152,12 +221,10 @@ int main(int argc, char * argv[])
 
         wstring wfilepath = filepath;
         std::thread([hWnd, wfilepath]() {
-            auto hdc = GetDC(hWnd);
-
-            dealFrame(hdc, (char*)(w2s(wfilepath).c_str()), hWnd);
-
-            ReleaseDC(hWnd, hdc);
+            dealFrame((char*)(w2s(wfilepath).c_str()), hWnd);
         }).detach();
+
+        std::thread(DrawVideoLoop).detach();
     });
 
     return mainWind.Run();
