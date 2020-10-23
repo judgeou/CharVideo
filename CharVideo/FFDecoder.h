424dd0357135179849352e3964773d39e46549e0
judgeou <judgeou@vip.qq.com>
2020-10-23 18:05:48 +0800

    音频打开失败了

--- CharVideo/FFDecoder.h
+++ CharVideo/FFDecoder.h
@@ -18,12 +18,19 @@ extern "C" {
 static AVPixelFormat hw_pix_fmt;
 static AVBufferRef* hw_device_ctx;
 
+struct ResponseFrame {
+    AVFrame* videoFrame;
+    AVFrame* audioFrame;
+};
+
 class FFDecoder {
 public:
     AVRational timebase;
     double fps;
     int height;
     int width;
+    int audio_sample_rate;
+    int audio_channels;
 
 	FFDecoder(char * infile) {
 		auto hw_type = AV_HWDEVICE_TYPE_DXVA2;
@@ -35,16 +42,18 @@ public:
         for (unsigned int i = 0; i < pFormatContext->nb_streams; i++)
         {
             pLocalCodecParameters = pFormatContext->streams[i]->codecpar;
-            pLocalCodec = avcodec_find_decoder(pLocalCodecParameters->codec_id);
 
             if (pLocalCodecParameters->codec_type == AVMEDIA_TYPE_VIDEO) {
+                videoStreamIndex = i;
+
+                pVLocalCodec = avcodec_find_decoder(pLocalCodecParameters->codec_id);
                 timebase = pFormatContext->streams[i]->time_base;
 
                 for (int i = 0;; i++) {
-                    const AVCodecHWConfig* config = avcodec_get_hw_config(pLocalCodec, i);
+                    const AVCodecHWConfig* config = avcodec_get_hw_config(pVLocalCodec, i);
                     if (!config) {
                         fprintf(stderr, "Decoder %s does not support device type %s.\n",
-                            pLocalCodec->name, av_hwdevice_get_type_name(hw_type));
+                            pVLocalCodec->name, av_hwdevice_get_type_name(hw_type));
 
                     }
                     else {
@@ -56,7 +65,7 @@ public:
                     }
                 }
 
-                pCodecContext = avcodec_alloc_context3(pLocalCodec);
+                pCodecContext = avcodec_alloc_context3(pVLocalCodec);
                 avcodec_parameters_to_context(pCodecContext, pLocalCodecParameters);
 
                 pCodecContext->get_format = get_hw_format;
@@ -64,7 +73,7 @@ public:
                     // error
                 }
 
-                avcodec_open2(pCodecContext, pLocalCodec, NULL);
+                avcodec_open2(pCodecContext, pVLocalCodec, NULL);
 
                 fps = av_q2d(pCodecContext->framerate);
                 width = pCodecContext->width;
@@ -74,53 +83,87 @@ public:
                 pFrame = av_frame_alloc();
                 sw_frame = av_frame_alloc();
             }
+            else if (pLocalCodecParameters->codec_type == AVMEDIA_TYPE_AUDIO) {
+                audioStreamIndex = i;
+
+                pALocalCodec = avcodec_find_decoder(pLocalCodecParameters->codec_id);
+                pACodecContext = avcodec_alloc_context3(pALocalCodec);
+                avcodec_open2(pACodecContext, pALocalCodec, NULL);
+                audio_sample_rate = pACodecContext->sample_rate;
+                audio_channels = pACodecContext->channels;
+                audioFrame = av_frame_alloc();
+            }
         }
 
 	}
 	~FFDecoder() {
         av_frame_free(&sw_frame);
+        av_frame_free(&audioFrame);
         av_frame_free(&pFrame);
         av_packet_free(&pPacket);
         av_buffer_unref(&pCodecContext->hw_device_ctx);
+        avcodec_free_context(&pACodecContext);
         avcodec_free_context(&pCodecContext);
 
 		avformat_close_input(&pFormatContext);
 		avformat_free_context(pFormatContext);
 	}
 
-    AVFrame* RequestFrame() {
+    ResponseFrame RequestFrame() {
         while (av_read_frame(pFormatContext, pPacket) >= 0) {
-            avcodec_send_packet(pCodecContext, pPacket);
-            auto ret = avcodec_receive_frame(pCodecContext, pFrame);
-            if (ret != 0) {
-                av_packet_unref(pPacket);
-                continue;
-            }
+            if (pPacket->stream_index == videoStreamIndex) {
+                avcodec_send_packet(pCodecContext, pPacket);
+                auto ret = avcodec_receive_frame(pCodecContext, pFrame);
+                if (ret == 0) {
+                    if (pFrame->format == hw_pix_fmt) {
+                        /* retrieve data from GPU to CPU */
+                        if ((ret = av_hwframe_transfer_data(sw_frame, pFrame, 0)) < 0) {
+                            fprintf(stderr, "Error transferring the data to system memory\n");
+                            continue;
+                        }
+                        sw_frame->best_effort_timestamp = pFrame->best_effort_timestamp;
+                        sw_frame->pts = pFrame->pts;
+                    }
 
-            if (pFrame->format == hw_pix_fmt) {
-                /* retrieve data from GPU to CPU */
-                if ((ret = av_hwframe_transfer_data(sw_frame, pFrame, 0)) < 0) {
-                    fprintf(stderr, "Error transferring the data to system memory\n");
-                    continue;
+                    return { sw_frame, 0 };
                 }
-                sw_frame->best_effort_timestamp = pFrame->best_effort_timestamp;
-                sw_frame->pts = pFrame->pts;
+                else {
+                    av_packet_unref(pPacket);
+                }
             }
-
-            return sw_frame;
+            else if (pPacket->stream_index == audioStreamIndex) {
+                auto ret = avcodec_send_packet(pACodecContext, pPacket);
+                ret = avcodec_receive_frame(pACodecContext, audioFrame);
+                if (ret == 0) {
+                    return { 0, audioFrame };
+                }
+                else {
+                    av_packet_unref(pPacket);
+                }
+            }
         }
-        return nullptr;
+        return {};
+    }
+
+    AVCodecContext* GetACodecContext() {
+        return pACodecContext;
     }
 
 private:
 	AVFormatContext* pFormatContext;
     AVCodecParameters* pLocalCodecParameters;
-    AVCodec* pLocalCodec;
+    AVCodec* pVLocalCodec;
+    AVCodec* pALocalCodec;
     AVCodecContext* pCodecContext = nullptr;
+    AVCodecContext* pACodecContext = nullptr;
     
     AVPacket* pPacket;
     AVFrame* pFrame;
     AVFrame* sw_frame;
+    AVFrame* audioFrame;
+
+    int videoStreamIndex;
+    int audioStreamIndex;
 
     static enum AVPixelFormat get_hw_format(AVCodecContext* ctx, const enum AVPixelFormat* pix_fmts) {
         const enum AVPixelFormat* p;
--- CharVideo/SDLApp.cpp
+++ CharVideo/SDLApp.cpp
@@ -17,18 +17,42 @@ using std::shared_ptr;
 using std::make_shared;
 using namespace std::chrono;
 
+#define SDL_AUDIO_BUFFER_SIZE 1024;
+
+struct AVFrameList {
+    AVFrame pkt;
+    struct AVFrameList* next;
+};
+
+struct PacketQueue {
+    AVFrameList* first_frame, * last_frame;
+    int nb_packets;
+    int size;
+    SDL_mutex* mutex;
+    SDL_cond* cond;
+};
+
 SDL_Window* window;
 SDL_Renderer* renderer;
 SDL_Texture* texture;
 uint8_t* nv12buf;
 int xwidth;
 int xheight;
-
-int dealFrame(char* infile);
+bool isquit = false;
+shared_ptr<FFDecoder> ffdecoder = nullptr;
+auto startPlayTime = system_clock::now();
+PacketQueue audioQueue;
+int audioDeviceId;
 
 void PrintSDLErr() {
     auto err = SDL_GetError();
     printf("%s\n", err);
+}
+
+void packet_queue_init (PacketQueue* q) {
+    *q = {};
+    q->mutex = SDL_CreateMutex();
+    q->cond = SDL_CreateCond();
 }
 
 uint8_t* newNv12Plane(uint8_t* buf, uint8_t* data[8], int height, int linesize[8]) {
@@ -42,22 +66,62 @@ uint8_t* newNv12Plane(uint8_t* buf, uint8_t* data[8], int height, int linesize[8
     return buf;
 }
 
-void SDLPlayFrame(AVFrame* pFrame) {
-    if (texture == nullptr) {
-        texture = SDL_CreateTexture(renderer,
-            SDL_PIXELFORMAT_NV12,
-            SDL_TEXTUREACCESS_STREAMING,
-            pFrame->width,
-            pFrame->height);
+void SDLPlayFrame(ResponseFrame response) {
+    if (response.videoFrame) {
+        auto pFrame = response.videoFrame;
+        
+        int microsec = pFrame->pts * av_q2d(ffdecoder->timebase) * 1000000;
+        auto playTime = startPlayTime + microseconds(microsec);
+        std::this_thread::sleep_until(playTime);
+
+        if (texture == nullptr) {
+            texture = SDL_CreateTexture(renderer,
+                SDL_PIXELFORMAT_NV12,
+                SDL_TEXTUREACCESS_STREAMING,
+                pFrame->width,
+                pFrame->height);
+        }
+
+        newNv12Plane(nv12buf, pFrame->data, pFrame->height, pFrame->linesize);
+        SDL_UpdateTexture(texture, NULL, nv12buf, pFrame->linesize[0]);
+
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+        SDL_RenderPresent(renderer);
+    }
+    if (response.audioFrame) {
+        auto frame = response.audioFrame;
+        if (audioDeviceId >= 0) {
+            auto datasize = av_samples_get_buffer_size(NULL, 2, 48000, ffdecoder->GetACodecContext()->sample_fmt, 1);
+            SDL_QueueAudio(audioDeviceId, frame->data[0], datasize);
+        }
     }
 
-    newNv12Plane(nv12buf, pFrame->data, pFrame->height, pFrame->linesize);
-    SDL_UpdateTexture(texture, NULL, nv12buf, pFrame->linesize[0]);
+}
 
-    SDL_RenderClear(renderer);
-    SDL_RenderCopy(renderer, texture, nullptr, nullptr);
-    SDL_RenderPresent(renderer);
+void PlayNewFile(char* file) {
+    SDL_DestroyTexture(texture);
+    texture = nullptr;
+    ffdecoder = nullptr;
+    ffdecoder = make_shared<FFDecoder>(file);
 
+    if (nv12buf != 0) {
+        delete[] nv12buf;
+    }
+    nv12buf = new uint8_t[ffdecoder->width * ffdecoder->height * 1.5];
+
+    SDL_AudioSpec audioSpec, audioRealSpec;
+    audioSpec.freq = 48000;
+    audioSpec.format = AUDIO_S16SYS;
+    audioSpec.channels = ffdecoder->audio_channels;
+    audioSpec.silence = 0;
+    audioSpec.samples = SDL_AUDIO_BUFFER_SIZE;
+    audioSpec.callback = nullptr;
+    audioSpec.userdata = ffdecoder->GetACodecContext();
+
+    audioDeviceId = SDL_OpenAudioDevice(NULL, 0, &audioSpec, &audioRealSpec, SDL_AUDIO_ALLOW_ANY_CHANGE);
+
+    startPlayTime = system_clock::now();
 }
 
 int CreateSDLWindow() {
@@ -78,23 +142,13 @@ int CreateSDLWindow() {
 
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
-    auto startPlayTime = system_clock::now();
-
-    shared_ptr<FFDecoder> ffdecoder = nullptr;
-
-    bool isquit = false;
     SDL_Event event;
     while (!isquit) {
         int ret;
         if (ffdecoder) {
             ret = SDL_PollEvent(&event);
-            auto frame = ffdecoder->RequestFrame();
-            int microsec = frame->pts * av_q2d(ffdecoder->timebase) * 1000000;
-            
-            auto playTime = startPlayTime + microseconds(microsec);
-            std::this_thread::sleep_until(playTime);
-            
-            SDLPlayFrame(frame);
+            auto response = ffdecoder->RequestFrame();
+            SDLPlayFrame(response);
         }
         else {
             ret = SDL_WaitEvent(&event);
@@ -107,17 +161,7 @@ int CreateSDLWindow() {
 
             }
             else if (event.type == SDL_DROPFILE) {
-                SDL_DestroyTexture(texture);
-                texture = nullptr;
-                ffdecoder = nullptr;
-                ffdecoder = make_shared<FFDecoder>(event.drop.file);
-                
-                if (nv12buf != 0) {
-                    delete[] nv12buf;
-                }
-                nv12buf = new uint8_t[ffdecoder->width * ffdecoder->height * 2];
-                
-                startPlayTime = system_clock::now();
+                PlayNewFile(event.drop.file);
             }
             else if (event.type == SDL_WINDOWEVENT) {
                 switch (event.window.event) {
