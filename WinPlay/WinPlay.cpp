0b9af075e68d76ada5147aaf3b4e834b79521798
judgeou <judgeou@vip.qq.com>
2020-12-04 23:04:34 +0800

    视频和声音都有了，使用锁同步

--- WinPlay/WinPlay.cpp
+++ WinPlay/WinPlay.cpp
@@ -3,6 +3,8 @@
 #include <map>
 #include <mutex>
 #include <condition_variable>
+#include <Windows.h>
+#include <d3d9.h>
 
 using std::map;
 using std::vector;
@@ -38,18 +40,82 @@ struct AudioDataBuffer {
 	int read;
 };
 
+void UpdateScreen(HWND hwnd, AVFrame* frame) {
+	IDirect3DSurface9* surface = (IDirect3DSurface9*)frame->data[3];
+	if (surface == 0) {
+		return;
+	}
+	IDirect3DDevice9* d3d9device = 0;
+	surface->GetDevice(&d3d9device);
+
+	IDirect3DSwapChain9* swap;
+	d3d9device->GetSwapChain(0, &swap);
+	D3DPRESENT_PARAMETERS params;
+	swap->GetPresentParameters(&params);
+
+	RECT rect;
+	GetClientRect(hwnd, &rect);
+
+	if (params.BackBufferWidth != rect.right || params.BackBufferHeight != rect.bottom) {
+		D3DPRESENT_PARAMETERS presentParams = {};
+		presentParams.BackBufferWidth = rect.right;
+		presentParams.BackBufferHeight = rect.bottom;
+		presentParams.SwapEffect = D3DSWAPEFFECT_FLIP;
+		presentParams.Windowed = TRUE;
+		d3d9device->Reset(&presentParams);
+	}
+
+	IDirect3DSurface9* backSurface = 0;
+	d3d9device->GetBackBuffer(0, 0, D3DBACKBUFFER_TYPE_MONO, &backSurface);
+	if (backSurface) {
+		double srcRatio = (double)frame->width / frame->height;
+		double destRatio = (double)rect.right / rect.bottom;
+
+		RECT backRect = { 0, 0 };
+		if (srcRatio < destRatio) {
+			backRect.bottom = rect.bottom;
+			auto newWidth = rect.bottom * srcRatio;
+			auto offset = rect.right - newWidth;
+			backRect.right = newWidth + offset / 2;
+			backRect.left = offset / 2;
+		}
+		else if (srcRatio > destRatio) {
+			backRect.right = rect.right;
+			auto newHeight = rect.right / srcRatio;
+			auto offset = rect.bottom - newHeight;
+			backRect.bottom = newHeight + offset / 2;
+			backRect.top = offset / 2;
+		}
+		else {
+			backRect.bottom = rect.bottom;
+			backRect.right = rect.right;
+		}
+
+		auto ret = d3d9device->StretchRect(surface, NULL, backSurface, &backRect, D3DTEXF_LINEAR);
+		backSurface->Release();
+
+		ret = d3d9device->Present(NULL, NULL, hwnd, NULL);
+	}
+}
+
 int main(int argc, char** argv)
 {
 	static bool isPlay = true;
-	static mutex mutex_;
-	static condition_variable cond_;
-	static unique_lock<mutex> mlock(mutex_);
+
+	static mutex mutex_update;
+	static condition_variable cond_update;
+	// static unique_lock<mutex> mlock_update(mutex_update);
+
+	static mutex mutex_decode;
+	static condition_variable cond_decode;
+	// static unique_lock<mutex> mlock_decode(mutex_decode);
 
 	static int audioStreamIndex = -1;
 	static int videoStraemIndex = -1;
 	static AVCodecContext* acodecCtx = nullptr;
 	static AVCodecContext* vcodecCtx = nullptr;
 	AVBufferRef* hw_device = nullptr;
+	static AVFrame* videoFrame = 0;
 
 	// 打开视频容器
 	static AVFormatContext* avfCtx = 0;
@@ -81,7 +147,6 @@ int main(int argc, char** argv)
 	}
 
 	AudioDataBuffer audioDataBuffer;
-	audioDataBuffer.read = 0;
 
 	static map<AVSampleFormat, ma_format> ma_format_map = {
 		{ AV_SAMPLE_FMT_S16, ma_format_s16 },
@@ -94,6 +159,10 @@ int main(int argc, char** argv)
 	config.playback.channels = acodecCtx->channels;               // Set to 0 to use the device's native channel count.
 	config.sampleRate = acodecCtx->sample_rate;           // Set to 0 to use the device's native sample rate.
 	config.dataCallback = [](ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount) {
+		if (isPlay == false) {
+			return;
+		}
+
 		auto size = frameCount * pDevice->playback.channels * ma_get_bytes_per_sample(pDevice->playback.format);
 		auto audioDataBuffer = (AudioDataBuffer*)pDevice->pUserData;
 		auto& data = audioDataBuffer->data;
@@ -106,7 +175,6 @@ int main(int argc, char** argv)
 				if (ret != 0) {
 					// 结束播放
 					isPlay = false;
-					cond_.notify_all();
 					return;
 				}
 
@@ -162,6 +230,11 @@ int main(int argc, char** argv)
 						av_frame_free(&frame);
 					}
 					else if (ret == 0) {
+						videoFrame = frame;
+						cond_update.notify_all();
+						unique_lock<mutex> mlock_decode(mutex_decode);
+						cond_decode.wait(mlock_decode);
+
 						av_frame_free(&frame);
 						av_packet_free(&packet);
 						break;
@@ -179,16 +252,60 @@ int main(int argc, char** argv)
 	};   // This function will be called when miniaudio needs more data.
 	config.pUserData = &audioDataBuffer;   // Can be accessed from the device object (device.pUserData).
 
-	ma_device device;
+	static ma_device device;
 	if (ma_device_init(NULL, &config, &device) != MA_SUCCESS) {
 		return -1;  // Failed to initialize the device.
 	}
 
+	// 创建窗口
+	SetProcessDPIAware();
+	auto hInstance = GetModuleHandle(NULL);
+	auto className = L"winplay";
+
+	WNDCLASSEX wc = {};
+	wc.cbSize = sizeof(wc);
+	wc.hInstance = hInstance;
+	wc.lpszClassName = className;
+	wc.lpfnWndProc = [](HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) -> LRESULT {
+		switch (msg) {
+		case WM_DESTROY:
+			isPlay = false;
+			PostQuitMessage(0);
+			break;
+		default:
+			return DefWindowProc(hwnd, msg, wParam, lParam);
+		}
+		return 0;
+	};
+	RegisterClassEx(&wc);
+
+	static auto hwnd = CreateWindowEx(
+		NULL,
+		className,
+		L"title",
+		WS_OVERLAPPEDWINDOW,
+		1, 1, 1920, 1080,
+		NULL, NULL, hInstance, NULL
+	);
+	ShowWindow(hwnd, SW_SHOW);
+
 	ma_device_start(&device);     // The device is sleeping by default so you'll need to start it manually.
 
+	MSG msg;
 	while (isPlay) {
-		cond_.wait(mlock);
+		unique_lock<mutex> mlock_update(mutex_update);
+		cond_update.wait(mlock_update);
+
+		while (PeekMessage(&msg, hwnd, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		UpdateScreen(hwnd, videoFrame);
+
+		cond_decode.notify_all();
 	}
+
+	ma_device_uninit(&device);
 
 	avcodec_free_context(&vcodecCtx);
 	avcodec_free_context(&acodecCtx);
